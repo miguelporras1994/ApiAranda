8e92848d63909785038012049312fe8eec78fbbe
LENOVO <LENOVO@MIGUELPORRAS>
2023-01-28 11:14:47 -0500

    Agregar README.md.

---
+++ DataAcccesLayer/Models/ModelContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAcccesLayer.Models;
+
+public partial class ModelContext : DbContext
+{
+    public ModelContext()
+    {
+    }
+
+    public ModelContext(DbContextOptions<ModelContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Product__3214EC07C168003A");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Category)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Description)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
